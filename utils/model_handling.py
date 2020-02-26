308b105e544a03081c9613a8145fcbb2b175301f
Dylan <dpaiton@gmail.com>
2020-02-26 15:48:31 +0100

    can now use kwargs for activation functions

--- utils/model_handling.py
+++ utils/model_handling.py
@@ -6,7 +6,7 @@ Authors: Santiago Cadena, Dylan Paiton
 
 import numpy as np
 
-def get_normalized_activations(model, target_neuron_ids, contour_dataset, get_activation_function):
+def get_normalized_activations(model, target_neuron_ids, contour_dataset, get_activation_function, activation_function_kwargs={}):
     """
     contour_dataset should have shape [num_target_neurons][num_comparisons_per_target][num_datapoints, datapoint_length]
     Parameters:
@@ -20,7 +20,8 @@ def get_normalized_activations(model, target_neuron_ids, contour_dataset, get_ac
         activity_sub_list = []
         for comparison_index, datapoints in enumerate(contour_dataset[target_index]):
             num_images = datapoints.shape[0]
-            activations = get_activation_function(model, datapoints, neuron_index)
+            activations = get_activation_function(model, np.squeeze(datapoints), neuron_index,
+                **activation_function_kwargs)
             activity_max = np.amax(np.abs(activations))
             activations = activations / (activity_max + 0.00001)
             activations = activations.reshape(int(np.sqrt(num_images)), int(np.sqrt(num_images)))
