d652657ed3abe47aa86cb42e083ad7219428cf7e
Matthias Kümmmerer <matthias.kuemmerer@bethgelab.org>
2021-10-01 11:20:17 +0200

    tests for new coordinate selection

--- tests/test_curvature.py
+++ tests/test_curvature.py
@@ -105,6 +105,7 @@ def test_coordinate_transform_sphere(dimensions, radius):
     np.testing.assert_allclose(np.abs(np.prod(iso_curvatures)), expected_gaussian_curvature)
 
 
+@pytest.mark.skip("Will usually fail because the transformation reults in bad implicit functions")
 def test_coordinate_transform():
     f = QuadraticFunction(np.array([1.0, 2.0, 0.1, 0.5])).to(DEVICE)
     point = torch.tensor(np.hstack((np.zeros(3), [1]))).to(DEVICE)
@@ -123,3 +124,58 @@ def test_coordinate_transform():
     iso_curvatures2 = iso_curvatures2.detach().cpu().numpy()
 
     np.testing.assert_allclose(iso_curvatures1, iso_curvatures2)
+
+
+@pytest.mark.parametrize('dimensions', [20, 40, 60, 500])
+@pytest.mark.parametrize('radius', [0.1, 1, 2])
+def test_bad_condition(dimensions, radius):
+    f_3d = QuadraticFunction([3.0, 3.0, 1.0]+[0.5] * (dimensions - 3)).to(DEVICE)
+    point = torch.tensor(
+        [np.sqrt(0.5), np.sqrt(0.5), 0]+[0] * (dimensions - 3),
+        dtype=torch.float
+    ).to(DEVICE) * radius
+
+    # first in original coordinates
+    value_orig, pt_grad_orig, pt_hess_orig = value_grad_hess(f_3d, point)
+    with pytest.raises(ValueError):
+        iso_shape_operator_orig, iso_curvatures_orig, iso_directions_orig = \
+            curve_utils.local_response_curvature_isoresponse_surface(
+                pt_grad_orig,
+                pt_hess_orig,
+                coordinate_transformation=torch.eye(len(point)).to(DEVICE)
+            )
+
+    rst = None
+    # This will in general result in very bad conditioning
+    M = ortho_group.rvs(len(point), random_state=rst)
+    coordinate_transformation = torch.tensor(M).to(DEVICE)
+
+    def new_f(x):
+        return f_3d(torch.matmul(coordinate_transformation.T.type(x.dtype), x))
+    new_point = torch.matmul(
+        coordinate_transformation.type(point.dtype),
+        point
+    ).detach().clone()
+
+    value_new, pt_grad_new, pt_hess_new = value_grad_hess(new_f, new_point)
+
+    np.testing.assert_allclose(
+        value_orig.cpu().numpy(),
+        value_new.cpu().numpy(),
+        rtol=1e-6
+    )
+
+    iso_shape_operator_new, iso_curvatures_new, iso_directions_new = \
+        curve_utils.local_response_curvature_isoresponse_surface(
+            pt_grad_new, pt_hess_new,
+        )
+
+    expected_curvatures = np.array(
+        [-1/6] * (dimensions - 3) + [-1/3, -1.0]
+    ) / radius
+
+    np.testing.assert_allclose(
+        iso_curvatures_new.cpu().numpy(),
+        expected_curvatures,
+        rtol=1e-6,
+    )
